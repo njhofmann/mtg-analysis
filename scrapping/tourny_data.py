f4e24f3918dbb28f196d3d42c82341facbdbc8a3
N8theGr8 <inatejh@gmail.com>
2019-09-15 11:57:04 -0400

    Added optional tournament size parameter, added tournament id, redid tournament_info pk

--- scrapping/tourny_data.py
+++ scrapping/tourny_data.py
@@ -11,10 +11,11 @@ import logging
 import psycopg2.errors
 
 # psycopg2 constants
-PSYCOPG2_UNIQUE_VIOLATION = 23505
+PSYCOPG2_UNIQUE_VIOLATION_CODE = 23505
 
 # regex constants
 EVENT_URL_REGEX = re.compile('event\?e=[0-9]+&f=[A-Z]+')
+EVENT_SIZE_REGEX = re.compile('[0-9]+ players')
 DECK_URL_REGEX = re.compile('\?e=[0-9]+&d=[0-9]+&f=[A-Z]+')
 DECK_ARCHETYPE_REGEX = re.compile('archetype?\?a=[0-9]+')
 
@@ -28,18 +29,18 @@ def init_logging():
         channel.setFormatter(formatter)
         logger.addHandler(channel)
     init_channel(logging.StreamHandler())
-    init_channel(logging.FileHandler('mtgtop8_scrapper.log'))
+    init_channel(logging.FileHandler('mtgtop8_scrapper.log', mode='w'))
     return logger
 
 
 def get_and_wait(url, data=None):
     """
-    Retrieves the data from the given url and returns a BeautifulSoup parser on the HTML the url response returns. Puts
+    Retrieves the data from the given search_url and returns a BeautifulSoup parser on the HTML the search_url response returns. Puts
     the thread to sleep for a random amount of seconds after retrieval to ensure that server being requested from isn't
     overwhelmed and that scrapping looks less conspicuous.
-    :param url: url to fetch data from
-    :param data: data to send with url, if desired
-    :return: HTML BeautifulSoup parser on the HTML text the request on url returns
+    :param url: search_url to fetch data from
+    :param data: data to send with search_url, if desired
+    :return: HTML BeautifulSoup parser on the HTML text the request on search_url returns
     """
     if data:
         url_data = requests.post(url=url, data=data)
@@ -57,6 +58,16 @@ def format_date(date):
     """
     parsed_date = datetime.datetime.strptime(date, '%d/%m/%y')
     return parsed_date.strftime('%m/%d/%Y')
+
+
+def get_size(event):
+    possible_texts = event.find_all(class_='S14')
+    for text in possible_texts:
+        size = EVENT_SIZE_REGEX.match(text.get_text())
+        if size:  # is not None
+            return int(size.group())
+    return None
+
 
 
 def get_entry_rank(parent_elements):
@@ -81,38 +92,47 @@ def card_in_mainboard(card):
     return True
 
 
-def insert_into_tournament_info(event_name, event_date, format, event_url, db_cursor, logger):
+def insert_into_tournament_info(event_name, event_date, event_format, size, event_url, db_cursor, logger):
     def insert_query_func():
-        insert_query = sql.SQL('INSERT INTO {} ({}, {}, {}, {}) VALUES (%s, %s, %s, %s)').format(
-            sql.Identifier('tournament_info'), sql.Identifier('name'), sql.Identifier('date'),
-            sql.Identifier('format'), sql.Identifier('url'))
-        db_cursor.execute(insert_query, (event_name, event_date, format, event_url))
+        insert_query = sql.SQL('INSERT INTO {} ({}, {}, {}, {}, {}) VALUES (%s, %s, %s, %s, %s)').format(
+            sql.Identifier('tournament_info'), sql.Identifier('name'), sql.Identifier('date'), sql.Identifier('format'),
+            sql.Identifier('size'), sql.Identifier('url'))
+        db_cursor.execute(insert_query, (event_name, event_date, event_format, size, event_url))
 
     warning_msg = 'Duplicate entry for tournament_info attempted, key {}, {}'.format(event_name, event_date)
     execute_query_pass_on_unique_violation(insert_query_func, logger, warning_msg)
 
 
-def insert_into_tournament_entry(event_name, event_date, deck_archetype, deck_placement, player_name, deck_name,
+def get_tournament_info_id(event_name, event_date, event_format, event_url, db_cursor):
+    tourny_id_query = sql.SQL('SELECT {} FROM {} WHERE {} = %s AND {} = %s AND {} = %s AND {} = %s').format(
+        sql.Identifier('tourny_id'), sql.Identifier('tournament_info'), sql.Identifier('name'), sql.Identifier('date'),
+        sql.Identifier('format'), sql.Identifier('url'))
+    print(tourny_id_query, (event_name, event_date, event_format, event_url))
+    db_cursor.execute(tourny_id_query, (event_name, event_date, event_format, event_url))
+    print(tourny_id_query)
+    return int(db_cursor.fetchone()[0])
+
+
+def insert_into_tournament_entry(tourny_id, deck_archetype, deck_placement, player_name, deck_name,
                                  placement_url, db_cursor, logger):
     def insert_query_func():
         insert_query = sql.SQL(
-            'INSERT INTO {} ({}, {}, {}, {}, {}, {}, {}) VALUES (%s, %s, %s, %s, %s, %s, %s)').format(
-            sql.Identifier('tournament_entry'), sql.Identifier('tournament'), sql.Identifier('date'),
-            sql.Identifier('archetype'), sql.Identifier('place'), sql.Identifier('player'), sql.Identifier('name'),
-            sql.Identifier('url'))
-        db_cursor.execute(insert_query, (event_name, event_date, deck_archetype, deck_placement, player_name, deck_name,
+            'INSERT INTO {} ({}, {}, {}, {}, {}, {}) VALUES (%s, %s, %s, %s, %s, %s)').format(
+            sql.Identifier('tournament_entry'), sql.Identifier('tourny_id'), sql.Identifier('archetype'),
+            sql.Identifier('place'), sql.Identifier('player'), sql.Identifier('deck_name'), sql.Identifier('url'))
+        db_cursor.execute(insert_query, (tourny_id, deck_archetype, deck_placement, player_name, deck_name,
                                          placement_url))
 
     warning_msg = 'Duplicate entry for tournament_entry attempted, for key ' \
-                  '{}, {}, {}, {}, {}'.format(event_name, event_date, deck_archetype, deck_placement, player_name)
+                  '{}, {}, {}, {}'.format(tourny_id, deck_archetype, deck_placement, player_name)
     execute_query_pass_on_unique_violation(insert_query_func, logger, warning_msg)
 
 
-def get_tournament_entry_id(event_name, event_date, deck_archetype, deck_placement, player_name, db_cursor):
-    entry_id_query = sql.SQL('SELECT {} FROM {} WHERE {} = %s AND {} = %s AND {} = %s AND {} = %s AND {} = %s').format(
-        sql.Identifier('entry_id'), sql.Identifier('tournament_entry'), sql.Identifier('tournament'),
-        sql.Identifier('date'), sql.Identifier('archetype'), sql.Identifier('place'), sql.Identifier('player'))
-    db_cursor.execute(entry_id_query, (event_name, event_date, deck_archetype, deck_placement, player_name))
+def get_tournament_entry_id(tourny_id, deck_archetype, deck_placement, player_name, db_cursor):
+    entry_id_query = sql.SQL('SELECT {} FROM {} WHERE {} = %s AND {} = %s AND {} = %s AND {} = %s').format(
+        sql.Identifier('entry_id'), sql.Identifier('tournament_entry'), sql.Identifier('tourny_id'),
+        sql.Identifier('archetype'), sql.Identifier('place'), sql.Identifier('player'))
+    db_cursor.execute(entry_id_query, (tourny_id, deck_archetype, deck_placement, player_name))
     return int(db_cursor.fetchone()[0])
 
 
@@ -132,25 +152,25 @@ def execute_query_pass_on_unique_violation(query_func, logger, warning_msg):
     try:
         query_func()
     except psycopg2.IntegrityError as e:
-        if e.pgcode != PSYCOPG2_UNIQUE_VIOLATION:
-            logger.warn(warning_msg)
+        if e.pgcode != PSYCOPG2_UNIQUE_VIOLATION_CODE:
+            logger.warning(warning_msg)
         else:
             raise e
 
 
-def retrieve_and_parse(url, format, database, user='postgres'):
+def retrieve_and_parse(search_url, url_format, database, user='postgres'):
     """
-    Given a url to a format webpage on mtgtop8.com, pulls all tournaments and their related placement info and loads
+    Given a url to a url_format webpage on mtgtop8.com, pulls all tournaments and their related placement info and loads
     them into the given Postgres database. Pulls all tournament info, placements in each tournament, cards played in
     each placement, etc.
-    :param url:
-    :param format: format being queried
+    :param search_url:
+    :param url_format: format being queried
     :param database: name of Postgres database
     :param user: login username for given database
     :return: None
     """
     logger = init_logging()
-    base_url = re.match('.*.com/', url).group()
+    base_url = re.match('.*.com/', search_url).group()
     page = 1
     min_event_count = 5
     with psycopg2.connect(user=user, dbname=database) as con:
@@ -158,7 +178,7 @@ def retrieve_and_parse(url, format, database, user='postgres'):
         with con.cursor() as cursor:
             while True:
                 child_page_value = {'cp': page}  # set page value
-                soup = get_and_wait(url, child_page_value)
+                soup = get_and_wait(search_url, child_page_value)
                 parents = soup.find_all(class_='hover_tr')  # combine parent.find() into lambda
                 events = [parent for parent in parents if parent.find(href=EVENT_URL_REGEX)]
 
@@ -179,24 +199,30 @@ def retrieve_and_parse(url, format, database, user='postgres'):
                     break
 
                 for event in normal_events:
+                    logger.info('Fetching for page {}'.format(page))
+                    page += 1
+
                     event_info = event.find(href=EVENT_URL_REGEX)
                     event_url = base_url + event_info['href']
                     event_name = event_info.get_text()
                     logger.info('Fetching event {} from {}'.format(event_name, event_url))
+                    size = get_size(event)
                     event_date = format_date(event.find(class_='S10').get_text())
-                    insert_into_tournament_info(event_name, event_date, format, event_url, cursor, logger)
-                    parse_event(event_name, event_date, event_url, base_url, cursor, logger)
-                    page += 1
+                    insert_into_tournament_info(event_name, event_date, url_format, size, event_url, cursor, logger)
+                    tourny_id = get_tournament_info_id(event_name, event_date, url_format, event_url, cursor)
+                    parse_event(tourny_id, event_name, event_date, event_url, base_url, cursor, logger)
 
 
-def parse_event(event_name, event_date, event_url, base_url, db_cursor, logger):
+def parse_event(tourny_id, event_name, event_date, event_url, base_url, db_cursor, logger):
     """
-    Given the url of a tournament on mtgtop8.com, pulls all decks that placed in the tournament and enters them into
+    Given the search_url of a tournament on mtgtop8.com, pulls all decks that placed in the tournament and enters them into
     the database of the given database cursor. Pulls player of each placement, the info of the deck they played, etc.
     :param event_name:
+    :param event_date:
     :param event_url:
     :param base_url:
     :param db_cursor:
+    :param logger:
     :return:
     """
     soup = get_and_wait(event_url)
@@ -210,12 +236,12 @@ def parse_event(event_name, event_date, event_url, base_url, db_cursor, logger):
         logger.info('Fetching tournament entry {} from {}'.format(deck_name, deck_url))
         deck_rank = get_entry_rank(parent)
         player_name = parent.find(class_='G11').get_text()
-        parse_entry(event_name, event_date, deck_url, deck_name, deck_rank, player_name, db_cursor, logger)
+        parse_entry(tourny_id, deck_url, deck_name, deck_rank, player_name, db_cursor, logger)
 
 
-def parse_entry(event_name, event_date, placement_url, deck_name, deck_placement, player_name, db_cursor, logger):
+def parse_entry(tourny_id, placement_url, deck_name, deck_placement, player_name, db_cursor, logger):
     """
-    Given the url to a tournament placement on mtgtop8.com, pull the ranked deck's info in the database of the given
+    Given the search_url to a tournament placement on mtgtop8.com, pull the ranked deck's info in the database of the given
     database cursor. Info such as played cards, card quantities, player name, ranking, etc.
     :param event_name:
     :param placement_url:
@@ -225,12 +251,17 @@ def parse_entry(event_name, event_date, placement_url, deck_name, deck_placement
     :return: None
     """
     url_data = get_and_wait(placement_url)
-    deck_archetype = url_data.find(href=DECK_ARCHETYPE_REGEX).get_text()
 
-    insert_into_tournament_entry(event_name, event_date, deck_archetype, deck_placement, player_name, deck_name,
-                                 placement_url, db_cursor, logger)
+    deck_archetype = url_data.find(href=DECK_ARCHETYPE_REGEX)
+    if deck_archetype is None:
+        deck_archetype = deck_name
+    else:
+        deck_archetype = deck_archetype.get_text()
 
-    entry_id = get_tournament_entry_id(event_name, event_date, deck_archetype, deck_placement, player_name, db_cursor)
+    insert_into_tournament_entry(tourny_id, deck_archetype, deck_placement, player_name, deck_name, placement_url,
+                                 db_cursor, logger)
+
+    entry_id = get_tournament_entry_id(tourny_id, deck_archetype, deck_placement, player_name, db_cursor)
 
     cards = url_data.find_all(class_='G14')
     for card in cards:
@@ -243,6 +274,8 @@ def parse_entry(event_name, event_date, placement_url, deck_name, deck_placement
 
 
 if __name__ == '__main__':
-    url = 'https://www.mtgtop8.com/format?f=MO&meta=44'
-    format = 'modern'
-    retrieve_and_parse(url, format, 'mtg_analysis')
+    urls_and_formats = [('https://www.mtgtop8.com/format?f=MO&meta=44', 'modern'),
+                        ('https://www.mtgtop8.com/format?f=LE&meta=16', 'legacy'),
+                        ('https://www.mtgtop8.com/format?f=ST&meta=58', 'standard')]
+    for url, url_format in urls_and_formats:
+        retrieve_and_parse(url, url_format, 'mtg_analysis')
